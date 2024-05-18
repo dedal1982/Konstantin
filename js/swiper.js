6ff570b56652c9485737f9d241b7f92b13d411f2
aldm <d44.a1@yandex.ru>
2024-05-18 21:42:36 +0300

    1

---
+++ js/swiper.js
@@ -0,0 +1,22 @@
+const swiperOne = new Swiper(".swiperOne", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
