3c90e78b4a5945cc17d866f271f88c81ccd93f3d
aldm <d44.a1@yandex.ru>
2024-05-19 01:05:22 +0300

    1

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const imageClick = document.querySelectorAll(".slide-item img");
 const page = document.querySelector(".page");
 const popup = document.querySelector(".popup");
 const popupImg = document.querySelector(".popup-content img");
+const popupClosed = document.querySelector(".popup-closed");
 
 imageClick.forEach((img) => {
   img.addEventListener("click", () => {
@@ -9,4 +10,9 @@ imageClick.forEach((img) => {
     popup.classList.add("popup-open");
     page.classList.add("lock");
   });
+});
+
+popupClosed.addEventListener("click", () => {
+  popup.classList.remove("popup-open");
+  page.classList.remove("lock");
 });
